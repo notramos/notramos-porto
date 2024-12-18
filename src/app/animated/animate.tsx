0aa44590b2149dd64ae8f70caf776a369ac41203
notramos <sergioguntur18@gmail.com>
2024-12-18 11:25:53 +0700

    fix commit

--- src/app/animated/animate.tsx
+++ src/app/animated/animate.tsx
@@ -1,40 +1,36 @@
-"use client"; // Direktif ini menandai file sebagai Client Component
-import React, { useState, useEffect } from "react";
+"use client";
+import React, { useState, useEffect, useMemo } from "react";
 
 const AnimatedText: React.FC = () => {
-  const roles = ["Web Developer", "Data Scientist", "Full Stack Engineer", "Quality Assurance"]; // Daftar teks
-  const [currentIndex, setCurrentIndex] = useState(0); // Indeks teks aktif
-  const [displayedText, setDisplayedText] = useState(""); // Teks yang ditampilkan saat ini
-  const [isDeleting, setIsDeleting] = useState(false); // Status penghapusan teks
+  const roles = useMemo(() => ["Web Developer", "Data Scientist", "Full Stack Engineer", "Quality Assurance"], []);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [displayedText, setDisplayedText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
-    const fullText = roles[currentIndex]; // Teks penuh dari indeks aktif
-    let typingSpeed = isDeleting ? 50 : 100; // Kecepatan ketikan (lebih cepat saat menghapus)
+    const fullText = roles[currentIndex];
+    const typingSpeed = isDeleting ? 50 : 100;
 
     const handleTyping = () => {
       if (!isDeleting && displayedText.length < fullText.length) {
-        // Menambahkan huruf satu per satu
         setDisplayedText(fullText.slice(0, displayedText.length + 1));
       } else if (isDeleting && displayedText.length > 0) {
-        // Menghapus huruf satu per satu
         setDisplayedText(fullText.slice(0, displayedText.length - 1));
       } else if (!isDeleting && displayedText.length === fullText.length) {
-        // Tunggu sebentar sebelum mulai menghapus
         setTimeout(() => setIsDeleting(true), 1000);
       } else if (isDeleting && displayedText.length === 0) {
-        // Beralih ke teks berikutnya setelah selesai menghapus
         setIsDeleting(false);
         setCurrentIndex((prevIndex) => (prevIndex + 1) % roles.length);
       }
     };
 
     const typingTimeout = setTimeout(handleTyping, typingSpeed);
-    return () => clearTimeout(typingTimeout); // Bersihkan timeout saat komponen unmount
+    return () => clearTimeout(typingTimeout);
   }, [displayedText, isDeleting, roles, currentIndex]);
 
   return (
-    <p className="text-lg text-white mt-2">
-      <span className="border-r-2 font-bold text-aquamarine pr-1 animate-blink">{displayedText}</span>
+    <p className="text-lg text-aquamarine mt-2">
+      <span className="border-r-2 border-white pr-1 animate-blink">{displayedText}</span>
     </p>
   );
 };
